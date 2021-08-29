043db5cfc0f412f4a99cbf6e1cc69f74eae0a22b
Brad Dikianidis <brad@adcanis.com>
2021-08-29 11:25:15 -0400

    added jsfile

---
+++ app.js
@@ -0,0 +1,6 @@
+const searchBtn = document.querySelector('.searchBtn');
+const title = document.querySelector('.title');
+
+searchBtn.addEventListener('click', function() {
+    title.style.color = '#292929';
+})
